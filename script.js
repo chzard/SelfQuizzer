7205c2758c70a50ed81c3f205766da6e2dbcf050
28elaineh <28elaineh@students.harker.org>
2025-04-27 23:18:07 -0700

    text & file inputs working on function scope

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
-var questionAnswer = new Map();
 console.log("init complete");
-
-//figure out input somehow?
 document.getElementById("questionInput").addEventListener("submit",questionInput);
-function questionInput(evt) {
+let textInput = "";
+let fileInput = "";
+
+function questionInput() {
     //testing: alert the value of each?
     console.log("Event determined");
-    const f = document.getElementById("questionFileInput").files[0];
-    const textInput = document.getElementById("questionTextInput").value;
-    let fileInput = "";
+    const file = document.getElementById("questionFileInput").files[0];
+    textInput = document.getElementById("questionTextInput").value;
 
     //read file input
-    if (f) {
-        var reader = new FileReader();
-        reader.readAsText(f);
-        fileInput = reader.result;
-        console.log("File read")
+    if (file) {
+        const reader = new FileReader();
+        reader.readAsText(file);
+        reader.onload = () => storeResults(reader.result);
+        function storeResults(result) {
+        fileInput = result;
+        }
+        reader.onerror = () => console.log("File input failed to read");
     } else {
-        console.log("Failed to load file input!");
+        console.log("File empty");
     }
 
-    if (f && textInput) {
-        console.log("Two inputs detected:\n" + fileInput + "\n" + textInput);
-    }
-    else if (f) {
-        console.log("File input:\n" + fileInput);
-    }
-    else if (textInput) {
-        console.log("Text input:\n" + textInput);
-    }
+    // if (fileInput && textInput) {
+    //     console.log("Two inputs detected:\n" + fileInput + "\n" + textInput);
+    // }
+    // else if (fileInput) {
+    //     console.log("File input:\n" + fileInput);
+    // }
+    // else if (textInput) {
+    //     console.log("Text input:\n" + textInput);
+    // }
 
 }
+console.log(fileInput);
+console.log(textInput); 
